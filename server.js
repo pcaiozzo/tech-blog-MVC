e32041033f6b59590c629c3df6f742625d9980a0
pcaiozzo <paigecaiozzo@gmail.com>
2022-07-18 19:58:54 -0400

    testing

--- server.js
+++ server.js
@@ -33,7 +33,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
 app.use(require("./controllers/"));
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-  connection.sync({ force: false });
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
