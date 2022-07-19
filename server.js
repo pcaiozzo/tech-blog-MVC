a1624a42c6ec1111410884e910ef54f6e082d878
pcaiozzo <paigecaiozzo@gmail.com>
2022-07-19 14:51:57 -0400

    fixed

--- server.js
+++ server.js
@@ -33,10 +33,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + "/public"));
 app.use(require("./controllers/"));
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log(
-    "Express server listening on port %d in %s mode",
-    this.address().port,
-    app.settings.env
-  );
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+  connection.sync({ force: false }); //.then(() => require('./seeds'))
 });
+
+// app.listen(process.env.PORT || 3000, function () {
+//   console.log(
+//     "Express server listening on port %d in %s mode",
+//     this.address().port,
+//     app.settings.env
+//   );
+// });
